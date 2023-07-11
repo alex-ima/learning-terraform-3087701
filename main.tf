5afe03325f3f7d087aa578e183d75ed33cd18070
alex-ima <139144614+alex-ima@users.noreply.github.com>
2023-07-11 13:14:03 -0600

    Fixed vpc

--- main.tf
+++ main.tf
@@ -51,8 +51,8 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.dev_vpc.vpc_id
-  subnets            = module.dev_vpc.public_subnets
+  vpc_id             = module.vpc.vpc_id
+  subnets            = module.vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = [
