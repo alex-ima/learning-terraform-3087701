6d32c727565e50937b63a5478a225f6abd1862f0
alex-ima <139144614+alex-ima@users.noreply.github.com>
2023-07-11 12:57:29 -0600

    re-added sample code

--- main.tf
+++ main.tf
@@ -20,8 +20,8 @@ module "vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2", "us-west-2"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 
   enable_nat_gateway = true
 
