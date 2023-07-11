a1ed8d95c05d072e9991a8491d203ec8e0084321
alex-ima <139144614+alex-ima@users.noreply.github.com>
2023-07-11 12:51:35 -0600

    no real changes

--- main.tf
+++ main.tf
@@ -45,8 +45,8 @@ vpc_security_group_ids = [module.blog_sg.security_group_id]
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name = "blog"
 
+  name   = "blog"
   vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
